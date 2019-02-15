1e3a7cac0d84e8d9b93bedf51a7927b06da89e45
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-14 22:45:53 -0200

    Initial commit

---
+++ src/main/kotlin/com/edandaniel/fcmapi/FcmapiApplication.kt
@@ -0,0 +1,12 @@
+package com.edandaniel.fcmapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcmapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FcmapiApplication>(*args)
+}
+
